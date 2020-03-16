1e65cf6c3fbef65bacdb94735085fc84507a9a98
shalini-p <shalini.p@sequoiaat.com>
2020-03-16 23:58:30 +0530

    added new files

---
+++ src/main/java/com/thinkaloud/blog/StreamsTest.java
@@ -0,0 +1,10 @@
+package com.thinkaloud.blog;
+
+public class StreamsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/thinkaloud/blog/JavaSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkaloud.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
